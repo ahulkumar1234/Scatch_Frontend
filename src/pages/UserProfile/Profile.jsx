805fe605486c2a0b3aee441d7239b8958e19776d
Rahul Kumar | Full Stack Developer <rahulkumar8340527941@gmail.com>
2025-12-24 17:16:51 +0530

    Update default profile image source to empty
    Replace the default profile image source with an empty string.

--- src/pages/UserProfile/Profile.jsx
+++ src/pages/UserProfile/Profile.jsx
@@ -116,7 +116,7 @@ const Profile = () => {
                     >
                         {/* Default image */}
                         <img
-                            src={'https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png'}
+                            src={''}
                             alt="Profile"
                             className="w-full h-full object-cover"
                         />
